cec8d1e893e366de3e0727666eaf18de378f4b4a
Vincent Wölfer <vincent.woelfer@gmail.com>
2025-09-30 11:58:42 +0200

    -- wip --

--- src/StencilViewport.gd
+++ src/StencilViewport.gd
@@ -12,12 +12,19 @@ func _ready() -> void:
 	self.canvas_cull_mask = (1 << 1)
 	parent_viewport.canvas_cull_mask = (1 << 0) # Ensure parent renders only layer 1
 
+	# Enable Stencil Layer on Main Viewport for testing
+	# parent_viewport.set_canvas_cull_mask_bit(1, true)
+
 	self.world_2d = parent_viewport.world_2d
 
+	# Size
 	self.size = window.size
 
+	print("Stencil Viewport size: ", self.size)
+	print("Window size: ", window.size)
 
-	print("Stencil Viewport got parent viewport: ", parent_viewport)
+
+	# print("Stencil Viewport got parent viewport: ", parent_viewport)
 
 
 func _process(delta: float) -> void:
