feaa080ffed88edf617b9eca524d0e5d3cea3f01
Vincent Wölfer <vincent.woelfer@gmail.com>
2025-10-14 12:43:17 +0200

    -- wip --

--- src/post_processing/PostProcessCanvasLayer.gd
+++ src/post_processing/PostProcessCanvasLayer.gd
@@ -3,8 +3,17 @@ class_name PostProcessCanvasLayer
 
 @onready var color_rect: ColorRect = $StencilColorRect
 
+var post_process_material: ShaderMaterial
+
 func _ready() -> void:
     color_rect.size = get_viewport().get_visible_rect().size # Windows size
+
+    post_process_material = color_rect.material as ShaderMaterial
+
+
+func _process(delta: float) -> void:
+    if post_process_material and Global.camera:
+        post_process_material.set_shader_parameter("zoom", Global.camera.zoom_curr)
 
 
 func update_size(new_size: Vector2) -> void:
