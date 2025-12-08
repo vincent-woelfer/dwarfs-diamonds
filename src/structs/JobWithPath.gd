5c3c6cc0621398eeb14aaee69720fcb8759577c1
Vincent Wölfer <vincent.woelfer@gmail.com>
2025-12-08 14:48:28 +0100

    -- wip --

--- src/structs/JobWithPath.gd
+++ src/structs/JobWithPath.gd
@@ -1,17 +1,9 @@
 class_name JobWithPath
 extends RefCounted
 
-var job: Job
-var path: Path
+var job: Job = null
+var path: Path = null
 
 func _init(job_: Job, path_: Path) -> void:
     job = job_
     path = path_
-
-
-# Ensure path is freed when this object is freed.
-# Necessary because this struct (RefCounted) has a Node2D as member.
-# func _notification(what: int) -> void:
-#     if what == NOTIFICATION_PREDELETE:
-#         if path and not path.is_queued_for_deletion():
-#             path.queue_free()
